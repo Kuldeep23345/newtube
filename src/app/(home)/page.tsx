42432b8b1bb8a21ebe782bffeb60d53eb64e1e77
Kuldeep <kunalrajput67718@gmail.com>
2026-01-05 16:44:59 +0530

    home layout done

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,3 @@
-import Image from "next/image";
-
 export default function page() {
-  return (
-    <div>
-      <Image src="/logo.svg" alt="Logo" width={100} height={100} />
-      <h1 className="text-3xl font-semibold tracking-tighter">newtube</h1>
-    </div>
-  );
+  return <div>i will upload videos in future</div>;
 }
