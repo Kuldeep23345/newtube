bafe37442f58c28671f4147fdcfbea6463e427b2
Kuldeep <kunalrajput67718@gmail.com>
2026-01-06 18:04:24 +0530

    Implement infinite scrolling for video content in the studio, utilizing new hook and component, and update studio view layout.

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,16 +1,38 @@
-'use client';
+"use client";
 
 import { trpc } from "@/trpc/client";
 import { DEFAULT_LIMIT } from "@/constants";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { InfiniteScroll } from "@/components/infinite-scroll";
 
-export default function VideosSection() {
-    const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery({
-     limit:DEFAULT_LIMIT,
-    },{getNextPageParam:(lastPage)=>lastPage.nextCursor});
+export const VideosSection = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const VideosSectionSuspense = () => {
+  const [data, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+    {
+      limit: DEFAULT_LIMIT,
+    },
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
 
   return (
     <div>
-       {JSON.stringify(data)}
+      {JSON.stringify(data)}
+
+      <InfiniteScroll
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
     </div>
-  )
-}
+  );
+};
--- src/modules/studio/ui/view/studio-view.tsx
+++ src/modules/studio/ui/view/studio-view.tsx
@@ -1,10 +1,16 @@
-import VideosSection from "../sections/videos-section";
-
+import { VideosSection } from "../sections/videos-section";
 
 export default function StudioView() {
   return (
-    <div>
-        <VideosSection/>
+    <div className="flex flex-col gap-y-6 pt-2.5">
+      <div className="px-4">
+        <h1 className="text-2xl font-bold">Channel content</h1>
+        <p className="text-xs text-muted-foreground">
+          Manage your channel content and videos
+        </p>
+      </div>
+
+      <VideosSection />
     </div>
-  )
+  );
 }
