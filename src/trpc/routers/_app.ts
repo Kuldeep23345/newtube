2260e23d296267c249779f06f34b238f64cc3ee2
Kuldeep <kunalrajput67718@gmail.com>
2026-01-10 13:57:51 +0530

    Implement video reactions functionality with like and dislike mutations, and integrate into video top row component

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,6 +3,7 @@ import {  createTRPCRouter } from "../init";
 import { studioRouter } from "@/modules/studio/server/procedures";
 import { videosRouter } from "@/modules/videos/server/procedures";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
+import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
 
 
 
@@ -11,6 +12,7 @@ export const appRouter = createTRPCRouter({
     videos:videosRouter,
     categories:categoriesRouter,
     videoViews:videoViewsRouter,
+    videoReactions:videoReactionsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
