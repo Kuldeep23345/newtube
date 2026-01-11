8905a9bdee8b8acec0f2d254049357e4a3df70b4
Kuldeep <kunalrajput67718@gmail.com>
2026-01-11 14:12:15 +0530

    infinite comments procedures  done

--- src/modules/comments/server/procedures.ts
+++ src/modules/comments/server/procedures.ts
@@ -6,7 +6,7 @@ import {
   protectedProcedure,
 } from "@/trpc/init";
 import z from "zod";
-import { eq, getTableColumns } from "drizzle-orm";
+import { and, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
 export const commentsRouter = createTRPCRouter({
   create: protectedProcedure
     .input(
@@ -33,18 +33,54 @@ export const commentsRouter = createTRPCRouter({
     .input(
       z.object({
         videoId: z.string(),
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            updatedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
       })
     )
     .query(async ({ input }) => {
-      const { videoId } = input;
+      const { videoId, cursor, limit } = input;
       const data = await db
         .select({
           ...getTableColumns(comments),
           user: users,
         })
         .from(comments)
-        .where(eq(comments.videoId, videoId))
-        .innerJoin(users, eq(comments.userId, users.id));
-      return data;
+        .where(
+          and(
+            eq(comments.videoId, videoId),
+            cursor
+              ? or(
+                  lt(comments.updatedAt, cursor.updatedAt),
+                  and(
+                    eq(comments.updatedAt, cursor.updatedAt),
+                    lt(comments.id, cursor.id)
+                  )
+                )
+              : undefined
+          )
+        )
+        .innerJoin(users, eq(comments.userId, users.id))
+        .orderBy(desc(comments.updatedAt), desc(comments.id))
+        .limit(limit + 1);
+      const hasMore = data.length > limit;
+      const items = hasMore ? data.slice(0, -1) : data;
+      const lastItem = items[items.length - 1];
+
+      const nextCursor = hasMore
+        ? {
+            id: lastItem.id,
+            updatedAt: lastItem.updatedAt,
+          }
+        : null;
+
+      return {
+        items,
+        nextCursor,
+      };
     }),
 });
