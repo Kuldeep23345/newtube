14e629d3629b216bde0f843f3633fb4bbf554d91
Kuldeep <kunalrajput67718@gmail.com>
2026-01-16 23:10:39 +0530

    Dynamically set APP_URL based on environment variables and client-side context.

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,8 @@
 export const DEFAULT_LIMIT = 5;
-export const APP_URL = process.env.VERCEL_URL || "http://localhost:3000";
- 
+export const APP_URL =
+  process.env.NEXT_PUBLIC_APP_URL ||
+  (typeof window !== "undefined"
+    ? window.location.origin
+    : process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000");
