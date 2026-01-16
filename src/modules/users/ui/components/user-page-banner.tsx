721ade49706f351a8f740925d368d5ab7d51a115
Kuldeep <kunalrajput67718@gmail.com>
2026-01-16 13:00:54 +0530

    Add banner upload modal and integrate it into the user page banner component.

--- src/modules/users/ui/components/user-page-banner.tsx
+++ src/modules/users/ui/components/user-page-banner.tsx
@@ -4,20 +4,27 @@ import { useAuth } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { Edit2Icon } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
+import { BannerUploadModal } from "./banner-upload-modal";
+import { useState } from "react";
 
 interface UserPageBannerProps {
   user: UserGetOneOutput;
 }
 
 export const UserPageBannerSkeleton = () => {
-  return <Skeleton className="w-full max-h-50 h-[15vh] md:h-[25vh]"/>
+  return <Skeleton className="w-full max-h-50 h-[15vh] md:h-[25vh]" />;
 };
-
 
 export const UserPageBanner = ({ user }: UserPageBannerProps) => {
   const { userId } = useAuth();
+  const [isBannerUploadModalOpen, setIsBannerUploadModalOpen] = useState(false);
   return (
     <div className="relative group">
+      <BannerUploadModal
+        userId={user.id}
+        open={isBannerUploadModalOpen}
+        onOpenChange={setIsBannerUploadModalOpen}
+      />
       <div
         className={cn(
           "w-full max-h-50 h-[15vh] md:h-[25vh] bg-linear-to-r from-gray-100 to-gray-200 rounded-xl ",
@@ -33,6 +40,7 @@ export const UserPageBanner = ({ user }: UserPageBannerProps) => {
           <Button
             type="button"
             size="icon"
+            onClick={() => setIsBannerUploadModalOpen(true)}
             className="absolute top-4 right-4 rounded-full bg-black/50 hover:bg-black/50 opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity duration-500"
           >
             <Edit2Icon className="size-4 text-white" />
